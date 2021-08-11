a68bcc4adcade07b00c8bc13f4beef2408e95f3b
sh0guni <donburij@gmail.com>
2021-08-11 21:12:49 +0300

    implement new, Add and Mul for Color

--- src/color.rs
+++ src/color.rs
@@ -1,3 +1,5 @@
+use std::ops::{Add, Div, Mul, Neg, Sub};
+
 #[derive(Debug, Copy, Clone, PartialEq)]
 pub struct Color {
     pub r: f64,
@@ -10,4 +12,43 @@ pub fn write_color(color: Color) {
     let ig = (255.999f64 * color.g) as u32;
     let ib = (255.999f64 * color.b) as u32;
     println!("{} {} {}", ir, ig, ib);
+}
+
+impl Add for Color {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        Self {
+            r: self.r + other.r,
+            g: self.g + other.g,
+            b: self.b + other.b,
+        }
+    }
+}
+
+impl Mul<f64> for Color {
+    type Output = Self;
+
+    fn mul(self, rhs: f64) -> Self::Output {
+        Self {
+            r: self.r * rhs,
+            g: self.g * rhs,
+            b: self.b * rhs,
+        }
+    }
+}
+
+impl Mul<Color> for f64 {
+    type Output = Color;
+
+    fn mul(self, rhs: Color) -> Color {
+        rhs * self
+    }
+}
+
+
+impl Color {
+    pub fn new(r: f64, g: f64, b: f64) -> Self {
+        Self { r, g, b }
+    }
 }
