3965f0bc343fb4f58b7b010e389a9dc7b0628a6c
sh0guni <donburij@gmail.com>
2021-08-30 22:47:57 +0300

    add simple diffusion to sphere

--- src/main.rs
+++ src/main.rs
@@ -13,14 +13,18 @@ use crate::hittable::Hittable;
 use crate::hittable_list::HittableList;
 use crate::ray::Ray;
 use crate::sphere::Sphere;
-use crate::vec3::{Point3, Vec3};
+use crate::vec3::{random_in_unit_sphere, Point3, Vec3};
 use rand::distributions::Standard;
 use rand::{thread_rng, Rng};
 use std::io::{self, Write};
 
-fn ray_color(r: &Ray, world: &impl Hittable) -> Color {
+fn ray_color(r: &Ray, world: &impl Hittable, depth: usize) -> Color {
+    if depth == 0 {
+        return Color::new(0.0, 0.0, 0.0);
+    }
     if let Some(rec) = world.hit(r, 0.0, f64::INFINITY) {
-        return 0.5 * (rec.normal + Color::new(1.0, 1.0, 1.0));
+        let target = rec.p + rec.normal + random_in_unit_sphere();
+        return 0.5 * ray_color(&Ray::new(rec.p, target - rec.p), world, depth - 1);
     }
     let unit_direction = r.direction.unit_vector();
     let t = 0.5 * (unit_direction.y + 1.0);
@@ -29,10 +33,11 @@ fn ray_color(r: &Ray, world: &impl Hittable) -> Color {
 
 fn main() {
     // Image
-    let aspect_ratio = 16.0 / 9.0;
-    let image_width = 400.0;
-    let image_height = (image_width / aspect_ratio) as u32;
-    let samples_per_pixel = 100;
+    const ASPECT_RATIO: f64 = 16.0 / 9.0;
+    const IMAGE_WIDTH: f64 = 400.0;
+    const IMAGE_HEIGHT: u32 = (IMAGE_WIDTH / ASPECT_RATIO) as u32;
+    const SAMPLES_PER_PIXEL: usize = 100;
+    const MAX_DEPTH: usize = 50;
 
     // World
     let world = HittableList {
@@ -47,24 +52,24 @@ fn main() {
 
     // Render
 
-    println!("P3\n{} {}\n255", image_width, image_height);
+    println!("P3\n{} {}\n255", IMAGE_WIDTH, IMAGE_HEIGHT);
 
-    for j in (0..image_height).rev() {
+    for j in (0..IMAGE_HEIGHT).rev() {
         eprint!("\rScanlines remaining: {} ", j);
         io::stdout().flush().unwrap();
-        for i in 0..image_width as u32 {
+        for i in 0..IMAGE_WIDTH as u32 {
             let pixel_color = thread_rng()
                 .sample_iter::<(f64, f64), &Standard>(&Standard)
-                .take(samples_per_pixel)
+                .take(SAMPLES_PER_PIXEL)
                 .map(|(ir, ij)| {
-                    let u = (i as f64 + ir) / (image_width - 1.0);
-                    let v = (j as f64 + ij) / (image_height as f64 - 1.0);
+                    let u = (i as f64 + ir) / (IMAGE_WIDTH - 1.0);
+                    let v = (j as f64 + ij) / (IMAGE_HEIGHT as f64 - 1.0);
                     let r = cam.get_ray(u, v);
-                    ray_color(&r, &world)
+                    ray_color(&r, &world, MAX_DEPTH)
                 })
                 .fold(Color::new(0.0, 0.0, 0.0), |acc, c| acc + c);
 
-            write_color(pixel_color, samples_per_pixel);
+            write_color(pixel_color, SAMPLES_PER_PIXEL);
         }
     }
     eprintln!("Done.");
