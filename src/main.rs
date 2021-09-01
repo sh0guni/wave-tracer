6b9dccd38ef272b94d53aebb239756e8a4f839c2
sh0guni <donburij@gmail.com>
2021-09-01 18:40:58 +0300

    add metal material

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ use crate::color::Color;
 use crate::diffusion::{random_in_hemisphere, random_in_unit_sphere, random_unit_vector};
 use crate::hittable::Hittable;
 use crate::hittable_list::HittableList;
-use crate::material::Lambertian;
+use crate::material::{Lambertian, Metal};
 use crate::ray::Ray;
 use crate::sphere::Sphere;
 use crate::vec3::{Point3, Vec3};
@@ -51,22 +51,33 @@ fn main() {
     let material_ground = Rc::new(Lambertian {
         albedo: Color::new(0.8, 0.8, 0.0),
     });
-
     let material_center = Rc::new(Lambertian {
         albedo: Color::new(0.7, 0.3, 0.3),
     });
+    let material_left = Rc::new(Metal::new(Color::new(0.8, 0.8, 0.8), 0.3));
+    let material_right = Rc::new(Metal::new(Color::new(0.8, 0.6, 0.2), 1.0));
 
     let world = HittableList {
         objects: vec![
+            Box::new(Sphere {
+                center: Point3::new(0.0, -100.5, -1.0),
+                radius: 100.0,
+                material: material_ground,
+            }),
             Box::new(Sphere {
                 center: Point3::new(0.0, 0.0, -1.0),
                 radius: 0.5,
                 material: material_center,
             }),
             Box::new(Sphere {
-                center: Point3::new(0.0, -100.5, -1.0),
-                radius: 100.0,
-                material: material_ground,
+                center: Point3::new(-1.0, 0.0, -1.0),
+                radius: 0.5,
+                material: material_left,
+            }),
+            Box::new(Sphere {
+                center: Point3::new(1.0, 0.0, -1.0),
+                radius: 0.5,
+                material: material_right,
             }),
         ],
     };
--- src/material.rs
+++ src/material.rs
@@ -1,7 +1,9 @@
+use crate::random_in_unit_sphere;
 use crate::hittable::HitRecord;
 use crate::random_unit_vector;
 use crate::Color;
 use crate::Ray;
+use crate::Vec3;
 use std::fmt::Debug;
 
 pub struct Scatter {
@@ -19,7 +21,7 @@ pub struct Lambertian {
 }
 
 impl Material for Lambertian {
-    fn scatter(&self, r_in: &Ray, rec: &HitRecord) -> Option<Scatter> {
+    fn scatter(&self, _r_in: &Ray, rec: &HitRecord) -> Option<Scatter> {
         let scatter_direction = rec.normal + random_unit_vector();
 
         // Catch degenerate scatter direction
@@ -33,5 +35,40 @@ impl Material for Lambertian {
             scattered: Ray::new(rec.p, corrected_scatter_direction),
             attenuation: self.albedo,
         })
+    }
+}
+
+fn reflect(v: Vec3, n: Vec3) -> Vec3 {
+    v - 2.0 * v.dot(&n) * n
+}
+
+#[derive(Debug, Copy, Clone)]
+pub struct Metal {
+    albedo: Color,
+    fuzz: f64,
+}
+
+impl Metal {
+    pub fn new(albedo: Color, fuzz: f64) -> Self {
+        Self {
+            albedo,
+            fuzz: if fuzz < 1.0 { fuzz } else { 1.0 },
+        }
+    }
+}
+
+impl Material for Metal {
+    fn scatter(&self, r_in: &Ray, rec: &HitRecord) -> Option<Scatter> {
+        let reflected = reflect(r_in.direction.unit_vector(), rec.normal);
+        let scattered = Ray::new(rec.p, reflected + self.fuzz * random_in_unit_sphere());
+
+        if scattered.direction.dot(&rec.normal) > 0.0 {
+            Some(Scatter {
+                scattered,
+                attenuation: self.albedo,
+            })
+        } else {
+            None
+        }
     }
 }
