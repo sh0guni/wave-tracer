3efe0967afcd4fb777de2bba26e769c3dcc61d02
sh0guni <donburij@gmail.com>
2021-09-06 18:01:35 +0300

    add final scene

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,10 @@ mod ray;
 mod sphere;
 mod util;
 mod vec3;
+use crate::color::get_pixel;
 use crate::camera::Camera;
-use crate::color::write_color;
 use crate::color::Color;
-use crate::diffusion::{random_in_hemisphere, random_in_unit_sphere, random_unit_vector};
+use crate::diffusion::{random_in_unit_sphere, random_unit_vector};
 use crate::hittable::Hittable;
 use crate::hittable_list::HittableList;
 use crate::material::Material;
@@ -19,11 +19,11 @@ use crate::material::{Dielectric, Lambertian, Metal};
 use crate::ray::Ray;
 use crate::sphere::Sphere;
 use crate::vec3::{Point3, Vec3};
-use rand::distributions::Standard;
+use rand::distributions::{Distribution, Standard, Uniform};
 use rand::{thread_rng, Rng};
-use std::f64::consts::PI;
 use std::io::{self, Write};
 use std::rc::Rc;
+use std::sync::{Arc, Mutex};
 
 fn ray_color(r: &Ray, world: &impl Hittable, depth: usize) -> Color {
     if depth == 0 {
@@ -40,61 +40,97 @@ fn ray_color(r: &Ray, world: &impl Hittable, depth: usize) -> Color {
     return (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0);
 }
 
+fn random_scene() -> HittableList {
+    let mut objects: Vec<Box<dyn Hittable>> = Vec::new();
+
+    let mut rng = thread_rng();
+    let metal_between = Uniform::from(0.5..1.0);
+
+    let ground_material = Rc::new(Lambertian {
+        albedo: Color::new(0.5, 0.5, 0.5),
+    });
+    objects.push(Box::new(Sphere {
+        center: Point3::new(0.0, -1000.0, 0.0),
+        radius: 1000.0,
+        material: ground_material,
+    }));
+
+    let p = Point3::new(4.0, 0.2, 0.0);
+
+    for a in -11..11 {
+        for b in -11..11 {
+            let (choose_mat, x, z) = rng.gen::<(f64, f64, f64)>();
+            let center = Point3::new(a as f64 + 0.9 * x, 0.2, b as f64 + 0.9 * z);
+
+            if (center - p).length() > 0.9 {
+                let sphere_material: Rc<dyn Material> = if choose_mat < 0.8 {
+                    let (r1, g1, b1, r2, g2, b2) = rng.gen();
+                    // diffuse
+                    let albedo = Color::new(r1, g1, b1) * Color::new(r2, g2, b2);
+                    Rc::new(Lambertian { albedo })
+                } else if choose_mat < 0.95 {
+                    // metal
+                    let albedo = Color::new(
+                        metal_between.sample(&mut rng),
+                        metal_between.sample(&mut rng),
+                        metal_between.sample(&mut rng),
+                    );
+                    let fuzz = rng.gen_range(0.5..1.0);
+                    Rc::new(Metal::new(albedo, fuzz))
+                } else {
+                    //glass
+                    Rc::new(Dielectric { ir: 1.5 })
+                };
+                objects.push(Box::new(Sphere {
+                    center,
+                    radius: 0.2,
+                    material: sphere_material,
+                }));
+            }
+        }
+    }
+
+    objects.push(Box::new(Sphere {
+        center: Point3::new(0.0, 1.0, 0.0),
+        radius: 1.0,
+        material: Rc::new(Dielectric { ir: 1.5 }),
+    }));
+
+    objects.push(Box::new(Sphere {
+        center: Point3::new(-4.0, 1.0, 0.0),
+        radius: 1.0,
+        material: Rc::new(Lambertian {
+            albedo: Color::new(0.4, 0.2, 0.1),
+        }),
+    }));
+
+    objects.push(Box::new(Sphere {
+        center: Point3::new(4.0, 1.0, 0.0),
+        radius: 1.0,
+        material: Rc::new(Metal::new(Color::new(0.7, 0.6, 0.5), 1.0)),
+    }));
+
+    HittableList { objects }
+}
+
 fn main() {
     // Image
-    const ASPECT_RATIO: f64 = 16.0 / 9.0;
-    const IMAGE_WIDTH: f64 = 400.0;
+    const ASPECT_RATIO: f64 = 3.0 / 2.0;
+    const IMAGE_WIDTH: f64 = 1200.0;
     const IMAGE_HEIGHT: u32 = (IMAGE_WIDTH / ASPECT_RATIO) as u32;
-    const SAMPLES_PER_PIXEL: usize = 100;
+    const SAMPLES_PER_PIXEL: usize = 500;
     const MAX_DEPTH: usize = 50;
 
     // World
 
-    let material_ground = Rc::new(Lambertian {
-        albedo: Color::new(0.8, 0.8, 0.0),
-    });
-    let material_center = Rc::new(Lambertian {
-        albedo: Color::new(0.1, 0.2, 0.5),
-    });
-    let material_left: Rc<dyn Material> = Rc::new(Dielectric { ir: 1.5 });
-    let material_right = Rc::new(Metal::new(Color::new(0.8, 0.6, 0.2), 1.0));
-
-    let world = HittableList {
-        objects: vec![
-            Box::new(Sphere {
-                center: Point3::new(0.0, -100.5, -1.0),
-                radius: 100.0,
-                material: material_ground,
-            }),
-            Box::new(Sphere {
-                center: Point3::new(0.0, 0.0, -1.0),
-                radius: 0.5,
-                material: material_center,
-            }),
-            Box::new(Sphere {
-                center: Point3::new(-1.0, 0.0, -1.0),
-                radius: 0.5,
-                material: Rc::clone(&material_left),
-            }),
-            Box::new(Sphere {
-                center: Point3::new(-1.0, 0.0, -1.0),
-                radius: -0.4,
-                material: material_left,
-            }),
-            Box::new(Sphere {
-                center: Point3::new(1.0, 0.0, -1.0),
-                radius: 0.5,
-                material: material_right,
-            }),
-        ],
-    };
+    let world = random_scene();
 
     // Camera
-    let look_from = Point3::new(3.0, 3.0, 2.0);
-    let look_at = Point3::new(0.0, 0.0, -1.0);
+    let look_from = Point3::new(13.0, 2.0, 3.0);
+    let look_at = Point3::new(0.0, 0.0, 0.0);
     let vup = Vec3::new(0.0, 1.0, 0.0);
-    let dist_to_focus = (look_from - look_at).length();
-    let aperture = 2.0;
+    let dist_to_focus = 10.0;
+    let aperture = 0.1;
 
     let cam = Camera::new(
         look_from,
